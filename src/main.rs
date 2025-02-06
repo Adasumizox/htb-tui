f7fecb83870d069ebde72aaa723f59a4ec1c3a08
Daniel Jeczen <jeczen.daniel@gmail.com>
2025-02-07 00:06:03 +0100

    Added ratatui and made list with states

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
 use std::{error::Error, io, env};
 use serde::{Deserialize, Serialize};
-use serde::ser::StdError;
 use serde_json::Value;
-use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
+use reqwest::Client;
 use ratatui::{
-    buffer::Buffer,
-    layout::Rect,
-    style::Stylize,
-    symbols::border,
-    text::{Line, Text},
-    widgets::{Block, Paragraph, Widget},
-    DefaultTerminal, Frame,
+    prelude::*,
+    backend::{Backend, CrosstermBackend},
+    layout::{Constraint, Layout},
+    style::{Color, Modifier, Style},
+    text::{Span, Line},
+    widgets::{Block, Borders, List, ListItem, ListState, Paragraph},
+    Frame, Terminal,
+};
+use crossterm::{
+    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode, KeyEventKind},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
 
 const HTB_API_URL: &str = "https://labs.hackthebox.com/api/v4";
@@ -51,191 +55,197 @@ struct Machine {
     active: Value,
 }
 
-#[derive(Debug, Default)]
-pub struct App {
-    counter: u8,
-    exit: bool,
+impl Machine {
+    fn is_active(&self) -> bool {
+        match &self.active {
+            Value::Bool(b) => *b,
+            Value::Number(n) => n.as_i64() == Some(1),
+            _ => false
+        }
+    }
+}
+
+struct App {
+    machines: Vec<Machine>,
+    state: ListState,
+    htb_api_key: String, // Hackthebox application key
+    client: Client, // Reqwest client instance
+    info_message: String, // Message for user
 }
 
 impl App {
-    pub fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
-        while !self.exit {
-            terminal.draw(|frame| self.draw(frame))?;
-            self.handle_events()?;
-        }
-        Ok(())
+    // Create new application and accept Hackthebox application key
+    async fn new(htb_api_key: String) -> Result<App, Box<dyn Error>> {
+        let client = reqwest::Client::new();
+        let machines = fetch_all_machines(&client, &htb_api_key).await?;
+        Ok(App {
+            machines,
+            state: ListState::default(),
+            htb_api_key,
+            client,
+            info_message: String::new(),
+        })
     }
 
-    fn draw(&self, frame: &mut Frame) {
-        frame.render_widget(self, frame.area());
-    }
-
-    fn handle_events(&mut self) -> io::Result<()> {
-        match event::read()? {
-            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
-                self.handle_key_event(key_event)
+    fn next(&mut self) {
+        let i = match self.state.selected() {
+            Some(i) => {
+                if i >= self.machines.len() - 1 {
+                    0
+                } else {
+                    i + 1
+                }
             }
-            _ => {}
+            None => 0,
         };
-        Ok(())
+        self.state.select(Some(i));
     }
 
-    fn handle_key_event(&mut self, key_event: KeyEvent) {
-        match key_event.code {
-            KeyCode::Char('q') => self.exit(),
-            KeyCode::Left => self.decrement_counter(),
-            KeyCode::Right => self.increment_counter(),
-            _ => {}
-        }
+    fn previous(&mut self) {
+        let i = match self.state.selected() {
+            Some(i) => {
+                if i == 0 {
+                    self.machines.len() - 1
+                } else {
+                    i - 1
+                }
+            }
+            None => 0,
+        };
+        self.state.select(Some(i));
     }
 
-    fn exit(&mut self) {
-        self.exit = true;
-    }
-
-    fn increment_counter(&mut self) {
-        self.counter += 1;
-    }
-    
-    fn decrement_counter(&mut self) {
-        self.counter -= 1;
+    async fn spawn_machine(&mut self) -> Result<(), Box<dyn Error>> {
+        todo!();
     }
 }
 
-impl Widget for &App {
-    fn render(self, area: Rect, buf: &mut Buffer) {
-        let title: Line = Line::from(" Counter App ".bold());
-        let instructions: Line = Line::from(vec![
-            " Decrement ".into(),
-            "<Left>".blue().bold(),
-            " Increment ".into(),
-            "<Right>".blue().bold(),
-            " Quit ".into(),
-            "<Q> ".blue().bold(),
-        ]);
-        let block: Block = Block::bordered()
-            .title(title.centered())
-            .title_bottom(instructions.centered())
-            .border_set(border::THICK);
-        
-        let counter_text: Text = Text::from(vec![Line::from(vec![
-                "Value: ".into(),
-                self.counter.to_string().yellow(),
-        ])]);
+async fn fetch_all_machines(client: &Client, htb_api_key: &str) -> Result<Vec<Machine>, Box<dyn Error>> {
+    let mut all_machines = Vec::new();
 
-        Paragraph::new(counter_text)
-            .centered()
-            .block(block)
-            .render(area, buf);
+    // Fetch active machines
+    let url = format!("{}/machine/paginated?per_page=100", HTB_API_URL);
+    let res = fetch_machines(client, htb_api_key, &url).await?;
+    all_machines.extend(res.data);
+
+    // Fetch retired machines
+    let url = format!("{}/machine/list/retired/paginated?per_page=100", HTB_API_URL);
+    let mut res = fetch_machines(client, htb_api_key, &url).await?;
+    all_machines.extend(res.data);
+
+    while let Some(next_url) = res.links.next {
+        res = fetch_machines(client, htb_api_key, &next_url).await?;
+        all_machines.extend(res.data);
     }
+
+    Ok(all_machines)
 }
 
-//async fn fetch_active_machines(api_token: &str) -> Result<Machine, Box<dyn Error>> {
-//    let client = reqwest::Client::new();
-//    let url = format!("{}/machine/list", HTB_API_URL);
-//    let res = client
-//        .get(url)
-//        .header("Authorization", format!("Bearer {}", API_TOKEN))
-//        .send()
-//        .await?
-//        .json::<Machine>()
-//        .await?;
-
-    //println!("{?:}", res);
-//    Ok(res)
-//}
-
-fn print_type_of<T>(_: &T) {
-    println!("{}", std::any::type_name::<T>());
+async fn fetch_machines(client: &Client, htb_api_key: &str, url: &str) -> Result<Root, Box<dyn Error>> {
+    let res = client
+        .get(url)
+        .header("Authorization", format!("Bearer {}", htb_api_key))
+        .send()
+        .await?
+        .json::<Root>()
+        .await?;
+    Ok(res)
 }
 
 #[tokio::main]
 async fn main() ->Result<(), Box<dyn std::error::Error>> {
-    let HTB_API_KEY = env::var("HTB_API_KEY")?;
+    // Setup cross terminal
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
 
-    let client = reqwest::Client::new();
-    let url = format!("{}/machine/paginated?per_page=100", HTB_API_URL);
-    let res = client
-        .get(url)
-        .header("Authorization", format!("Bearer {}", HTB_API_KEY))
-        .send()
-        .await?
-        .json::<Root>()
-        .await?;
-    let active_machines: Vec<Machine> = res.data;
-    println!("{:?}", active_machines);
-    let url = format!("{}/machine/list/retired/paginated?per_page=100", HTB_API_URL);
-    let mut res = client
-        .get(url)
-        .header("Authorization", format!("Bearer {}", HTB_API_KEY))
-        .send()
-        .await?
-        .json::<Root>()
-        .await?;
-    let mut retired_machines: Vec<Machine> = res.data;
-    while res.links.next.is_some()
-    {
-        let url = res.links.next.unwrap();
-        res = client
-            .get(url)
-            .header("Authorization", format!("Bearer {}", HTB_API_KEY))
-            .send()
-            .await?
-            .json::<Root>()
-            .await?;
-        retired_machines.append(&mut res.data);
+    // Create application
+    let htb_api_key = env::var("HTB_API_KEY")?;
+    let app = App::new(htb_api_key).await;
+    let res = run_app(&mut terminal, app).await;
+
+    // Restore terminal
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
+
+    // Error handling
+    if let Err(err) = res {
+        println!("{:?}", err)
     }
-    println!("{:?}", retired_machines);
-    //let mut terminal = ratatui::init();
-    //let app_result = App::default().run(&mut terminal);
-    //ratatui::restore();
-    //app_result
+
     Ok(())
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use ratatui::style::Style;
+async fn run_app<B: Backend>(
+    terminal: &mut Terminal<B>, 
+    mut app: Result<App, Box<dyn std::error::Error>>
+) ->io::Result<()> {
+    let mut app_unwraped = app.unwrap();
+    loop {
+        terminal.draw(|f| ui(f, &mut app_unwraped))?;
 
-    #[test]
-    fn render() {
-        let app = App::default();
-        let mut buf = Buffer::empty(Rect::new(0, 0, 50, 4));
-
-        app.render(buf.area, &mut buf);
-
-        let mut expected = Buffer::with_lines(vec![
-            "┏━━━━━━━━━━━━━━━━━ Counter App ━━━━━━━━━━━━━━━━━━┓",
-            "┃                    Value: 0                    ┃",
-            "┃                                                ┃",
-            "┗━ Decrement <Left> Increment <Right> Quit <Q> ━━┛",
-        ]);
-        let title_style = Style::new().bold();
-        let counter_style = Style::new().yellow();
-        let key_style = Style::new().blue().bold();
-        expected.set_style(Rect::new(18, 0, 13, 1), title_style);
-        expected.set_style(Rect::new(28, 1, 1, 1), counter_style);
-        expected.set_style(Rect::new(13, 3, 6, 1), key_style);
-        expected.set_style(Rect::new(30, 3, 7, 1), key_style);
-        expected.set_style(Rect::new(43, 3, 4, 1), key_style);
-
-        assert_eq!(buf, expected);
+        if let Event::Key(key) = event::read()? {
+            if key.kind == KeyEventKind::Press {
+                match key.code {
+                    KeyCode::Char('q') => return Ok(()),
+                    KeyCode::Down => app_unwraped.next(),
+                    KeyCode::Up => app_unwraped.previous(),
+                    KeyCode::Enter => {
+                        let spawn_result = app_unwraped.spawn_machine().await;
+                        if let Err(e) = spawn_result {
+                            app_unwraped.info_message = format!("Error: {}", e);
+                        }
+                    }
+                    _ => {}
+                }
+            }
+        }
     }
+}
 
-    #[test]
-    fn handle_key_event() -> io::Result<()> {
-        let mut app = App::default();
-        app.handle_key_event(KeyCode::Right.into());
-        assert_eq!(app.counter, 1);
+fn ui(f: &mut Frame, app: &mut App) {
+    let chunks =
+        Layout::vertical([Constraint::Percentage(90), Constraint::Percentage(10)]).split(f.area());
 
-        app.handle_key_event(KeyCode::Left.into());
-        assert_eq!(app.counter, 0);
+    let items: Vec<ListItem> = app
+        .machines
+        .iter()
+        .map(|machine| {
+            let status = if machine.is_active() {
+                Span::styled("Active", Style::default().fg(Color::Green))
+            } else {
+                Span::styled("Inactive", Style::default().fg(Color::Red))
+            };
+            let line = Line::from(vec![
+                Span::raw(format!("{:15} ({:10}) ", machine.name, machine.os)),
+                status,
+            ]);
 
-        let mut app = App::default();
-        app.handle_key_event(KeyCode::Char('q').into());
-        assert!(app.exit);
+            ListItem::new(line).style(Style::default().fg(Color::White))
+        })
+        .collect();
 
-        Ok(())
-    }
+    let list = List::new(items)
+        .block(Block::default().borders(Borders::ALL).title("Machines"))
+        .highlight_style(
+            Style::default()
+                .fg(Color::Yellow)
+                .add_modifier(Modifier::BOLD),
+        )
+        .highlight_symbol("> ");
+
+    f.render_stateful_widget(list, chunks[0], &mut app.state);
+
+    let info_paragraph = Paragraph::new(app.info_message.clone())
+        .style(Style::default().fg(Color::LightCyan))
+        .block(Block::default().borders(Borders::ALL).title("Info"));
+
+    f.render_widget(info_paragraph, chunks[1]);
 }
